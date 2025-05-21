7f290f7deabd2ffefbe6284753d0fe437cd65116
Brian <596833+flizzer@users.noreply.github.com>
2025-05-21 00:04:49 -0400

    making the app responsive

--- src/main.js
+++ src/main.js
@@ -11,6 +11,9 @@ class App {
     #mesh_ = null
 
     constructor() {
+        window.addEventListener('resize', () => {
+            this.#onWindowResize_();
+        })
     }
 
     initialize() {
@@ -37,7 +40,29 @@ class App {
             })
         );
         this.#scene_.add(this.#mesh_);
+        
+        this.#onWindowResize_();
         this.#raf();
+    }
+
+    #onWindowResize_() {
+        const canvas = this.#threejs_.domElement;
+        const dpr = window.devicePixelRatio;
+        const w = window.innerWidth;
+        const h = window.innerHeight;
+        const aspect = w / h;
+        console.log(`Resizing window to ${w} x ${h}`);
+
+        canvas.style.width = w + 'px';
+         canvas.style.height = h + 'px';
+        this.#threejs_.setSize(w * dpr, h * dpr, false);
+
+        //generally don't want to use this as it hides what the actual resolution is and
+        //works too much like magic --bhd
+        //this.#threejs_.setPixelRatio(dpr);
+
+        this.#camera_.aspect = aspect;
+        this.#camera_.updateProjectionMatrix();
     }
 
     #raf() {
@@ -53,7 +78,9 @@ class App {
     
         //state updates
         this.#controls_.update(timeElapsed);
+        this.#mesh_.rotation.x += timeElapsed;
         this.#mesh_.rotation.y += timeElapsed;
+        this.#mesh_.rotation.z += timeElapsed;
     }
 
     #render_() {
